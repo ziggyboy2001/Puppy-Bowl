f6c78249ccd8bae454c344207ea43089051a09da
ziggyboy2001 <brentpurks1@icloud.com>
2023-02-17 09:46:00 -0600

    puppybowl 1.2

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -1,37 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 function Dogs(props) {
-const [players, setPlayers] = useState([])
 
-useEffect(() => {
-    async function getDogs(){
-        try {
-            const response = await fetch(
-              'https://fsa-puppy-bowl.herokuapp.com/api/2301-ftb-et-web-ft/players'
-            );
-            const result = await response.json();
-            console.log(result.data.players);
-            
-          } catch (err) {
-            console.error(err);
-          }
-    }
-    getDogs()
-},[])
-console.log(players)
+console.log(props)
     return (
-        <div>
-            I am the dog component
-            <table>
 
-<td>ID: </td>
-<td>Name: </td>
-<td>Breed: </td>
-<td>Status:</td>
-<td>*Image*</td>
-<button>More Info</button>
-</table>
-        </div>
+    props.players.map(puppy =>{
+        return <tr key={puppy.id}>
+            <td>{puppy.name}</td>
+            <td>{puppy.breed}</td>
+            <td>{puppy.status}</td>
+        </tr>
+    })
+
+
+
     );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { Navbar, Dogs } from "./";
 
+
 const Main = () => {
+const [players, setPlayers] = useState([])
+async function getDogs(){
+    try {
+        const response = await fetch(
+          'https://fsa-puppy-bowl.herokuapp.com/api/2301-ftb-et-web-ft/players'
+        );
+        const result = await response.json();
+        const puppies = result.data.players
+        console.log(puppies);
+        setPlayers(puppies);
+      } catch (err) {
+        console.error(err);
+      }
+}
+useEffect(() => {
+    getDogs()
+
+},[])
+
     return(
         <div id="main">
             <Navbar />
-        <Dogs />
+        <Dogs players={players}/>
 
         </div>
     )
 }
 
 export default Main
+//        <Dogs puppies = {puppies}/>
